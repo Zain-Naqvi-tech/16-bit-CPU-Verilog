0cdac21762edd5d6bc7eb269d0bad455f89d1c46
Zain Naqvi <zainnaqvi.200621@gmail.com>
2025-12-27 03:06:06 -0500

    Clean up tb_mux_2to1.v comments
    Removed unnecessary comments and metadata from testbench.

--- Test Benches/tb_mux_2to1.v
+++ Test Benches/tb_mux_2to1.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/26/2025 08:15:20 PM
-// Design Name: 
-// Module Name: tb_mux_2to1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_mux_2to1();
 

0f108ccbd46731d8a0f357b1a4568ce46a1e5c45
Zain Naqvi <zainnaqvi.200621@gmail.com>
2025-12-27 03:05:53 -0500

    Clean up comments in tb_dmux_1to2.v
    Removed unnecessary comments and metadata from the testbench file.

--- Test Benches/tb_dmux_1to2.v
+++ Test Benches/tb_dmux_1to2.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/26/2025 08:53:45 PM
-// Design Name: 
-// Module Name: tb_dmux_1to2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_dmux_1to2();
 

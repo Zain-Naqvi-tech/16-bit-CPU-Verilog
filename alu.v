b5e2ce9ba8b2c7d4dd8e7364ffdf6000d62c9742
Zain Naqvi <zainnaqvi.200621@gmail.com>
2025-12-27 03:02:15 -0500

    Clean up header comments in alu.v
    Removed header comments from alu.v

--- alu.v
+++ alu.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/27/2025 12:30:12 AM
-// Design Name: 
-// Module Name: alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module alu(
     input [15:0] x,

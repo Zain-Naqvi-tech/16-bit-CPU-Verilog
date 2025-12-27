6f97692de1fd9fc6341076f07ed8973d0d02b2ea
Zain Naqvi <zainnaqvi.200621@gmail.com>
2025-12-27 03:02:56 -0500

    Clean up comments in mux_2to1.v
    Removed unnecessary comments and metadata from the mux_2to1.v file.

--- mux_2to1.v
+++ mux_2to1.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/26/2025 08:07:59 PM
-// Design Name: 
-// Module Name: mux_2to1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mux_2to1(
     input [15:0] a,

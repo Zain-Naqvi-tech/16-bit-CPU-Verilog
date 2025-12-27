38e4b8c0106b20e54875887fcde544ef211a0496
Zain Naqvi <zainnaqvi.200621@gmail.com>
2025-12-27 03:02:38 -0500

    Clean up comments in dmux_1to2.v
    Removed header comments from dmux_1to2.v

--- dmux_1to2.v
+++ dmux_1to2.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/26/2025 08:50:17 PM
-// Design Name: 
-// Module Name: dmux_1to2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module dmux_1to2(
     input in,

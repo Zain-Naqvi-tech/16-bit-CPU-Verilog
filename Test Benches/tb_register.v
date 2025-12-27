747338670ee721da61a33cd0fa454ed6163518f6
Zain Naqvi <zainnaqvi.200621@gmail.com>
2025-12-27 03:06:20 -0500

    Clean up comments in tb_register.v
    Removed unnecessary comments and metadata from the testbench file.

--- Test Benches/tb_register.v
+++ Test Benches/tb_register.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/27/2025 02:24:26 AM
-// Design Name: 
-// Module Name: tb_register
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_register();
 
